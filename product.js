8ce7cc25b57b89ddd889120f77bc7a25db615ee3
paresh951 <presh7047@outlook.com>
2024-06-09 19:43:54 +0530

    Product gallery

---
+++ product.js
@@ -0,0 +1,30 @@
+var productData = [
+    {
+        imgUrl: 'https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/22643790/2023/5/4/d596832c-cdc0-4da1-b202-f1da4995c1df1683192710220-Raymond-Men-Shirts-8111683192709462-1.jpg',
+        title: 'Casual Shirt',
+        description: 'Sample text. Click to select the textSample text. Click to select the textSample text. Click to select the textSample text. Click to select the textSample text. Click to select the textSample text. Click to select the text box. Click again or double click to start editing the text.',
+        price: 'Price - 1200rs',
+        availQuan: 'Available Quantity (15)'
+    },
+    {
+        imgUrl: 'https://rukminim1.flixcart.com/image/612/612/xif0q/shirt/b/d/f/3xl-13-lstr-wine-vtexx-original-imagnzbummhkgr7p.jpeg?q=70',
+        title: 'Slim Fit Shirt',
+        description: 'Sample text. Click to select the textSample text. Click to select the textSample text. Click to select the textSample text. Click to select the textSample text. Click to select the textSample text. Click to select the text box. Click again or double click to start editing the text.',
+        price: 'Price - 1000rs',
+        availQuan: 'Available Quantity (8)'
+    },
+    {
+        imgUrl: 'https://rukminim1.flixcart.com/image/612/612/kcc9q4w0/shirt/z/w/0/xl-hlsh011581-highlander-original-imafthkd6yksr6k8.jpeg?q=70',
+        title: 'Printed Shirt',
+        description: 'Sample text. Click to select the textSample text. Click to select the textSample text. Click to select the textSample text. Click to select the textSample text. Click to select the textSample text. Click to select the text box. Click again or double click to start editing the text.',
+        price: 'Price - 2500rs',
+        availQuan: 'Available Quantity (3)'
+    },
+    {
+        imgUrl: 'https://www.beyoung.in/api/cache/catalog/products/new_shirt_upload_21_10_2022/tan_brown_corduroy_shirt_for_men_base_3_11_2022_400x533.jpg',
+        title: 'Brown Shirt',
+        description: 'Sample text. Click to select the textSample text. Click to select the textSample text. Click to select the textSample text. Click to select the textSample text. Click to select the textSample text. Click to select the text box. Click again or double click to start editing the text.',
+        price: 'Price - 1800rs',
+        availQuan: 'Available Quantity (5)'
+    },
+]
